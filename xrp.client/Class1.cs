05fd17e7ca83c055efb33bdfb4ba9b894222ad2f
unknown <metscoder@gmail.com>
2020-11-09 00:19:06 -0500

    Initial commit

---
+++ xrp.client/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace xrp.client
+{
+    public class Class1
+    {
+    }
+}
---
+++ xrp.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xrp.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
